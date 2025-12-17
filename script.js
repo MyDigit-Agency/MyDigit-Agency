145f6035d459a733f73ac784c71d6dc0af90b3b8
Jonathan Myinda <myindaj@gmail.com>
2025-12-17 22:27:45 +0100

    feat: ajout du bouton WhatsApp flottant animé

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ overlay.onclick = () => {
     sideMenu.classList.remove("open");
     overlay.classList.remove("show");
 }
+
+const menu = document.getElementById("whatsapp-menu");
+
+function openWhatsappMenu() {
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+function closeWhatsappMenu() {
+  menu.style.display = "none";
+}
+
+function sendWhatsApp(service) {
+  const phone = "243980215272";
+  const message = `Bonjour MyDigit Agency,%0AJe suis intéressé par : ${service}`;
+  const url = `https://wa.me/${phone}?text=${message}`;
+  window.open(url, "_blank");
+  closeWhatsappMenu();
+}
